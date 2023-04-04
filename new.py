15fe948136dc934386250eaf36d84f821a557c01
ad1lovskiyy <mirakbarovodilbek8@gmail.com>
2023-04-04 20:02:11 +0500

    gid dasturni organdm

--- new.py
+++ new.py
@@ -1,1 +1,19 @@
-git init
+"""def isprime(son):
+    for i in range(2, son):
+        if son % i ==0:
+            return False
+    return True
+
+print(isprime(29))
+print(isprime(27))"""
+
+
+
+
+
+
+
+
+
+
+
